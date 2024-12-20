dca97d13b582e12634596a41ab2c3a5509022c1e
BlueXDrummer <adam@geoclap.com>
2024-12-20 15:14:47 +1100

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_socket.sendto("Hello, server!".encode('utf-8'), ('10.10.20.42', 12345))
+client_socket.sendto("Hello, server!".encode('utf-8'), ('0.0.0.0', 12345))
 
 client_socket.close()
